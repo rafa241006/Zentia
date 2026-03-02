8f90cb99a33e69ce4bd1161ab0cec7670efbd78d
Rafa Rizvi <rafa.20240499@iit.ac.lk>
2026-03-03 01:05:05 +0530

    created backend server

---
+++ zentia-backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import { connect } from 'mongoose'
+import {connectCloudinary} from './config/cloudinary.js'
+
+//app config
+
+const app= express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoint
+
+app.get('/', (req, res)=>{
+    res.send("API working yayay")
+})
+
+app.listen(port, ()=>  console.log("server started", port))
